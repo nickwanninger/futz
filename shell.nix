8562f35eefd6ff9a4dcfdaa132de6ddd54bf4597
Nick Wanninger <nickwanninger@gmail.com>
2023-09-11 15:22:05 -0500

    some more futzing

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} # here we import the nixpkgs package set   
+}:                                                                      
+pkgs.mkShell {               # mkShell is a helper function             
+  name="dev-environment";    # that requires a name                     
+  buildInputs = [            # and a list of packages                   
+    pkgs.cabal-install
+    pkgs.ghc
+  ];                                                                    
+  shellHook = ''             # bash to run when you enter the shell     
+    echo "Start developing..."                                          
+  '';                                                                   
+}
