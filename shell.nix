bdabf1a2092c95ae895ff91839cf35efc023a8f8
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2023-12-15 21:21:55 +0000

    reenable nix shell cabal and ghc packages

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
 pkgs.mkShell {               # mkShell is a helper function             
   name="dev-environment";    # that requires a name                     
   buildInputs = [            # and a list of packages                   
-    # pkgs.cabal-install
-    # pkgs.ghc
+    pkgs.cabal-install
+    pkgs.ghc
   ];                                                                    
 }
